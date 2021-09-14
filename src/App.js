e31e19cfc9711d5d1016db0b068c7105c8666c59
bridau19 <bridau19@gmail.com>
2021-09-14 08:56:36 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route
+} from "react-router-dom";
+
 import './App.css';
+import Article from '.containers/Article';
+import ArticleListing from "/containers/ArticleListing";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Switch>
+        <Route path="/article">
+          <Article/>
+        </Route>
+        <Route path="/">
+          <ArticleListing/>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
+/*
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +42,4 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+    */
