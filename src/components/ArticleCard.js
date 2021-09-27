65695f6894e661f7d6e4d447e2c6201b092b5741
bridau19 <bridau19@gmail.com>
2021-09-26 22:27:08 -0400

    finished Article and ArticleCard

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -2,6 +2,8 @@ import React from "react";
 import Article from "../containers/Article";
 
 function ArticleCard({article}) {
+    const newDate = new Date(article.publishedDate); // object type Date
+    const dateString = newDate.toDateString();
     return (
         <div className="ArticleCardWrapper">
             <div className="ArticleCardImage">
@@ -9,9 +11,11 @@ function ArticleCard({article}) {
             </div>
             <div className="ArticleCardText">
                 <h2 className="ArticleCardTitle">{article.title}</h2>
-                <p className="ArticleCardDate">{article.publishedDate}</p>
+                <p className="ArticleCardDate">{dateString}</p>
                 <p className="ArticleCardDate">{article.blurb}</p>
-                <p className="ArticleCardLink"><a href="#">Read More</a></p>
+                <p className="ArticleCardLink">
+                    <a href={`/article/${article.id}`}>Read More</a>
+                </p>
             </div>
         </div>
     );
